cfdba69e91ac852b38875c75e664d0afdd631d91
Oathan Rex <90545164+oathanrex@users.noreply.github.com>
2021-09-22 02:38:52 +0530

    Update character-count.js

--- character-count.js
+++ character-count.js
@@ -1,18 +1,13 @@
 /*
 ===============================================================
-
 Hi! Welcome to my little playground!
-
 My name is Oathan Rex. 'Open source' by default and always 'responsive',
 I'm a publicist, visual designer and frontend developer based in Barcelona. 
-
 Here you will find some of my personal experiments. Sometimes usefull,
 sometimes simply for fun. You are free to use them for whatever you want 
 but I would appreciate an attribution from my work. I hope you enjoy it.
-
 ===============================================================
 */
-
 /*
 ===================
 ===================
